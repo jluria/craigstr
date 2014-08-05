72b70294b58c3b0392706d2f23e4bb6a3c3609fd
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-05 11:49:08 -0400

    fix edit page

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  resources :regions
+
+  resource :dashboard, only: [:show]
+
+  root "homes#show"
+
+end
