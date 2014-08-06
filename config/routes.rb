7928f4aa1c0679633a911d93962221db753bdb8e
“Rafael <rafael.freaner@gmail.com>
2014-08-06 14:32:21 -0400

    Delete posts

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :posts, only: [:create, :edit, :update]
+  resources :posts, only: [:create, :edit, :update, :destroy]
   resources :regions do
     resources :categories, only: [:show]
   end
