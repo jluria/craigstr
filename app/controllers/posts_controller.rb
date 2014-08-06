0472a103313e51d7002eee2be030eee08fd8f78b
“Rafael <rafael.freaner@gmail.com>
2014-08-06 11:04:34 -0400

    Indent and partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,30 @@
 class PostsController < ApplicationController
 
-	def create
-	  post = Post.new(post_params)
-	  post.user = current_user
-	  post.save
-	  redirect_to dashboard_path
-	end
+  def create
+    post = Post.new(post_params)
+    post.user = current_user
+    post.save
+    redirect_to dashboard_path
+  end
 
-	private
+  def edit
+    @post = Post.find(params[:id]) 
+  end
 
-	def post_params
-		params.require(:post).permit(:title, :content)
-	end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params) #we are calling the method below in the private section
+      redirect_to dashboard_path
+    else
+      render:edit
+    end
+  end
+
+  private
+
+  def post_params
+        params.require(:post).permit(:title, :content)
+  end
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :edit, :update]
 
   resources :regions
 
