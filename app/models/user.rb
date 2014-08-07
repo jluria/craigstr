becbd2ef87950d8dc3b2c56120f48343c7852eb8
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-07 16:14:59 -0400

    Update new post route Limit deletation to post user or admin Update user model, assigns posted to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :posts
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def posted?(post)
+    posts.include?(post)
+  end
 end
