b054a2ac0b3c17e72c3d68fd0fa0da427e35a644
Charles Massry <charlesdmassry@gmail.com>
2014-08-07 15:16:51 -0400

    clean up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
-  def not_flagged(post)
-    spams.where(post: post, user: self).empty?
+  def not_flagged_as_spam?(post)
+    spams.where(post: post).empty?
   end
 end
