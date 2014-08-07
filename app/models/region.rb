30aa7852211900f3d28c249e4ebd38e8cec030af
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-07 11:35:45 -0400

    Create exclusion for new in model

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,7 @@
 class Region < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
+  validates :name, exclusion: { in: %w(new) }
+  validates :slug, exclusion: { in: %w(new) }
   after_create :generate_slug
   before_update :assign_slug
   has_many :posts
