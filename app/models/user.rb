4b6b80cb021e8c7fe0b433c1ab2e3de78c38cc8d
abinofbrooklyn <abin.abraham4@gmail.com>
2014-08-07 16:42:47 -0400

    Add and define method in model Update user conditional in post

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
+  def can_change?(post)
+    admin? || posted?(post)
+  end
+
+  private
+  
   def posted?(post)
-    post.user_id == id 
+    post.user_id == id
   end
 end
